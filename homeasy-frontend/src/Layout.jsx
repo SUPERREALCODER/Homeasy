0cf367c99f65338120559144bf4c91a4132c6f45
DarkXamper <krishguptano12@gmail.com>
2024-07-12 12:51:12 +0530

    adding some files and updates also sorted the routing for service page

--- homeasy-frontend/src/Layout.jsx
+++ homeasy-frontend/src/Layout.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import { Outlet } from'react-router-dom'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Outlet } from "react-router-dom";
 const Layout = () => {
   return (
     <>
-        <Header/>
-        <Outlet/>
-        <Footer/>
+      <Header />
+      <Outlet />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
