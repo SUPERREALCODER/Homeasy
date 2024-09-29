e8cf7d290528c9db2f0c0f7320e0fd46d5444fee
SUPERREALCODER <debabratag542@gmail.com>
2024-09-29 16:43:22 +0530

    chnages in use fetch user

--- homeasy-frontend/src/hooks/useFetchUser.jsx
+++ homeasy-frontend/src/hooks/useFetchUser.jsx
@@ -7,7 +7,7 @@ const useFetchUser = () => {
   useEffect(() => {
     async function fetchData() {
       try {
-        const result = await axios.get("http://localhost:3000/profile", {
+        const result = await axios.get("https://homeasy.onrender.com/profile", {
           withCredentials: true,
         });
         // console.log(result.data);
